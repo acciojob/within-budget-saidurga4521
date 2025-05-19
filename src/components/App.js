1d1f6587196e29383a604e1f0d4837a3b65e41b1
Durga Prasad <saikamireddi3@gmail.com>
2025-05-19 15:02:43 +0530

    chnages has done

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,24 @@ import "./../styles/App.css";
 
 const App = () => {
   const [data, setData] = useState([
-    { name: "Apple", price: 20, enable: false },
-    { name: "Banana", price: 10, enable: false },
-    { name: "Neutella", price: 30, enable: false },
-    { name: "Razor", price: 5, enable: false },
-    { name: "CornFlakes", price: 15, enable: false },
-    { name: "Sound Bar", price: 50, enable: false },
-    { name: "Iphone", price: 80, enable: false },
+    { name: "Apple", price: 20 },
+    { name: "Banana", price: 10 },
+    { name: "Neutella", price: 30 },
+    { name: "Razor", price: 5 },
+    { name: "CornFlakes", price: 15 },
+    { name: "Sound Bar", price: 50 },
+    { name: "Iphone", price: 80 },
   ]);
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const handleChange = (e) => {
     const value = Number(e.target.value);
     setInput(value);
-
-    const updatedData = data.map((item) => ({
-      ...item,
-      enable: item.price <= value,
-    }));
-    setData(updatedData);
   };
 
   return (
     <div>
-      <lable for="input">Enter your budget to check available items:</lable>
+      <h3>Enter your budget to check available items:</h3>
+
       <input type="Number" id="input" value={input} onChange={handleChange} />
       <h2>Items you can buy are in green color</h2>
       <table>
@@ -37,12 +32,13 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
+          {data.map((item) => (
+            <tr key={item.id}>
               <td>{item.name}</td>
               <td
                 style={{
-                  color: item.enable ? "rgb(0, 128, 0)" : "rgb(255, 0, 0)",
+                  color:
+                    item.price <= input ? "rgb(0, 128, 0)" : "rgb(255, 0, 0)",
                 }}
               >
                 {item.price}
